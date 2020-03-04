209eb006a46d992677477d0b090301872d93a1f9
kevin wen <wen122056@email.com>
2020-03-04 17:38:11 +0800

    调试ci

--- travis/add-key.sh
+++ travis/add-key.sh
@@ -5,9 +5,9 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-security import ./cert/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./cert/loginpush.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./cert/loginpush.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
+security import .travis/cert/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import .travis/cert/loginpush.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import .travis/cert/loginpush.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
 
 echo "list keychains: "
 security list-keychains
@@ -18,4 +18,4 @@ security find-identity -p codesigning  ~/Library/Keychains/ios-build.keychain
 echo " ****** "
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./cert/loginpush.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ".travis/cert/loginpush.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
