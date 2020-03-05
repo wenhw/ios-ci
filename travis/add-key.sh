cfd6e42a084c7deb2a19816648f82e42f0e3790a
kevin wen <wen122056@email.com>
2020-03-05 16:40:50 +0800

    修改add-key

--- travis/add-key.sh
+++ travis/add-key.sh
@@ -18,4 +18,4 @@ security find-identity -p codesigning  ~/Library/Keychains/ios-build.keychain
 echo " ****** "
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./travis/cert/loginpush.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./travis/cert/loginpush_pp.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
