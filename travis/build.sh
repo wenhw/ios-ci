074c8ab882f7db206b1762a06ca5a3614c65ecff
kevin wen <wen122056@email.com>
2020-03-06 11:55:47 +0800

    添加签名操作

--- travis/build.sh
+++ travis/build.sh
@@ -2,3 +2,5 @@
 xcrun xcodebuild -project LoginPush.xcodeproj -scheme LoginPush \
   -archivePath LoginPush.xcarchive archive
 
+xcrun xcodebuild -exportArchive -archivePath LoginPush.xcarchive \
+  -exportPath ./build -exportOptionsPlist ExportOptions.plist
