311c0ef3b8282bdfcce5b8fc30b74059e099ff39
kevin wen <wen122056@email.com>
2020-03-06 10:49:25 +0800

    调archive

--- travis/build.sh
+++ travis/build.sh
@@ -2,5 +2,3 @@
 xcrun xcodebuild -project LoginPush.xcodeproj -scheme LoginPush \
   -archivePath LoginPush.xcarchive archive
 
-xcrun xcodebuild -exportArchive -archivePath LoginPush.xcarchive \
-  -exportPath ./build -exportOptionsPlist ExportOptions.plist
